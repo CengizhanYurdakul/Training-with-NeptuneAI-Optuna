4bdcf6178f6c9e456a17d43c26bf5651800d9c4f
Cengizhan Yurdakul <cengizhanyurdakul97@gmail.com>
2022-09-13 15:20:32 +0300

    Structural change

--- Test/test_Trainer.py
+++
@@ -1,8 +0,0 @@
-from src.utils import *
-from src.Trainer import Trainer
-
-args = parseYaml("src/Mocking/options.yaml")
-
-def testTrainer():
-    trainer = Trainer(trial=None, args=args)
-    return trainer
